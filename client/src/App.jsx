f0d9c7ec1502202466afc8d8b260a42ac2c4b9d0
MoANsarMansoorie <moansarmansoorie@gmail.com>
2026-04-06 12:13:37 -0700

    Create Components and Pages folder in React project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Doctor from './pages/Doctor'
+import LogIn from './pages/LogIn'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+
 
 function App() {
 
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/doctor' element={<Doctor />} />
+        <Route path='/doctor/:speciality' element={<Doctor />} />
+        <Route path='/log-in' element={<LogIn />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
+    </div>
   )
 }
 
